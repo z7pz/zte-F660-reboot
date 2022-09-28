6c1446f2904e22176f048fbf15c7de8dbde4919f
z7pz <sdfygamer@gmail.com>
2022-09-28 17:17:27 +0300

    feat: Initialized 👏

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import puppeteer from "puppeteer";
+let ip = "10.25.168.50";
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://" + ip);
+
+  await page.evaluate(() => {
+    document.getElementById("Frm_Username")!.setAttribute("value", "admin");
+    document.getElementById("Frm_Password")!.setAttribute("value", "admin");
+  });
+
+  await page.click('input[type="submit"]');
+  await page.goto(`http://${ip}/start.ghtml`);
+  await page.evaluate(() => {
+    document.body
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("table")[0]
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tr")[0]
+      .getElementsByTagName("td")[0]
+      .getElementsByTagName("iframe")[1]
+      .contentDocument!.getElementById("Fnt_mmManager")!
+      .click();
+  });
+  await page.goto(`http://${ip}/start.ghtml`);
+  await page.evaluate(() => {
+    document.body
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("table")[0]
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tr")[0]
+      .getElementsByTagName("td")[0]
+      .getElementsByTagName("iframe")[1]
+      .contentDocument!.getElementsByClassName("h2_content")[2]
+      //@ts-ignore
+      .click();
+  });
+  await page.goto(`http://${ip}/start.ghtml`);
+  await page.evaluate(() => {
+    const my = document.body
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("table")[0]
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tr")[0]
+      .getElementsByTagName("td")[0]
+      .getElementsByTagName("iframe")[1].contentDocument!;
+    my.getElementById("Submit1")!.click();
+    my.getElementById("msgconfirmb")!.click();
+  });
+  await page.screenshot({ path: "last_screen.png" });
+  await browser.close();
+})();
